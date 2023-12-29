38cfbff82a56e5d375583c60f04ce2008bce1881
Marcio Fagundes <145873508+marciofag@users.noreply.github.com>
2023-12-29 18:20:22 -0300

    desafio

---
+++ index.js
@@ -0,0 +1,27 @@
+// Variáveis
+let nomeDoHeroi = "Batman"; // Você pode substituir pelo nome do seu herói
+let experienciaDoHeroi = 7500; // Você pode substituir pela XP do seu herói
+
+// Estrutura de decisão
+let nivelDoHeroi;
+
+if (experienciaDoHeroi < 1000) {
+  nivelDoHeroi = "Ferro";
+} else if (experienciaDoHeroi <= 2000) {
+  nivelDoHeroi = "Bronze";
+} else if (experienciaDoHeroi <= 5000) {
+  nivelDoHeroi = "Prata";
+} else if (experienciaDoHeroi <= 7000) {
+  nivelDoHeroi = "Ouro";
+} else if (experienciaDoHeroi <= 8000) {
+  nivelDoHeroi = "Platina";
+} else if (experienciaDoHeroi <= 9000) {
+  nivelDoHeroi = "Ascendente";
+} else if (experienciaDoHeroi <= 10000) {
+  nivelDoHeroi = "Imortal";
+} else {
+  nivelDoHeroi = "Radiante";
+}
+
+// Saída
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivelDoHeroi}`);
